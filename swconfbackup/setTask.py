2dd41c44ff73d267ded977d6e38d3b3333ad0250
arnohub <49862797+arnohub@users.noreply.github.com>
2021-04-02 13:48:58 +0800

    Update setTask.py

--- swconfbackup/setTask.py
+++ swconfbackup/setTask.py
@@ -12,8 +12,8 @@ working_dir = os.getcwd()
 for i in cron_manager:
     i = str(i)
     cron_list.append(i)
-if '0 21 * * * /bin/python3.9 {0}/swconf_backup.py >> {1}/swconf_backup.log 2>&1 &'.format(working_dir, working_dir) not in cron_list:
+if '0 2 * * * /bin/python3.9 {0}/swconf_backup.py >> {1}/swconf_backup.log 2>&1 &'.format(working_dir, working_dir) not in cron_list:
     job = cron_manager.new(command='/bin/python3.9 {0}/swconf_backup.py >> /swconf_backup/swconf_backup.log 2>&1 &'.format(working_dir, working_dir))
-    job.setall('0 21 * * *')
+    job.setall('0 2 * * *')
     cron_manager.write()
-print("定时备份配置任务已开启！将在每天21:00开始备份。")
+print("定时备份配置任务已开启！将在每天2:00开始备份。")
